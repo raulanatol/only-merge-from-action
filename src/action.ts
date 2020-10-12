50c115db5b87b6e4b0888f4320a2792ecc14150b
raulanatol <raul@natol.es>
2020-10-12 17:53:45 +0100

    Tests

--- src/action.ts
+++ src/action.ts
@@ -1,5 +1,11 @@
 import { context } from '@actions/github';
 
+
 export const start = async () => {
   console.log('CONTEXT', context);
+
+
+  console.log('REF', context.ref); // 'refs/heads/main'
+  // console.log('BASE_REF', context.base_ref);
+  // console.log('HEAD_REF', context.head_ref);
 };
